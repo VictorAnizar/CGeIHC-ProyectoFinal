8754b647b46c0b3740474dba93c11e543c808580
aleexmoon <alejandra.rdz01@comunidad.unam.mx>
2024-11-17 22:34:43 -0600

    Follow Camera vacio

--- Monopoly/main.cpp
+++ Monopoly/main.cpp
@@ -36,6 +36,7 @@
 #include "Shader_light.h"
 #include "Camera.h"
 #include "StaticCamera.h"
+#include "FollowCamera.h"
 #include "Texture.h"
 #include "Sphere.h"
 #include "Model.h" 
@@ -154,6 +155,7 @@ std::vector<Shader> shaderList;
 
 Camera mainCamera(glm::vec3(0.0f, 40.0f, 3.0f), glm::vec3(0.0f, 1.0f, 0.0f), -90.0f, 0.0f, 0.5f, 0.1f); 
 StaticCamera isoCamera(glm::vec3(10.0f, 30.0f, 10.0f), glm::vec3(0.0f, 0.0f, 1.0f), 5.0f, 0.1f);
+//FollowCamera followCamera(glm::vec3(0.0f, 5.0f, -10.0f), glm::vec3(0.0f, 1.0f, 0.0f), 5.0f, 0.1f);
 
 //Texturas tablero
 Texture AmTexture, AzTexture, RoTexture, VeTexture, pisoTexture;
